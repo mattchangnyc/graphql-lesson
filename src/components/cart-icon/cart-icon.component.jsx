8af7ac27ddb3f72221a45910d379c49da6520dd7
Matt Chang <mattchangnyc@gmail.com>
2020-12-20 22:19:11 -0500

    CartItem Count with Apollo

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -16,11 +12,4 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
 );
 
 
-const mapStateToProps = createStructuredSelector({
-  itemCount: selectCartItemsCount
-});
-
-export default connect(
-  mapStateToProps,
-  null
-)(CartIcon);
+export default CartIcon;
